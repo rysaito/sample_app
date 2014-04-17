f9d5f37a01612c7b92320140d8fe56f546b4030e
saito <t-rysaito@yahoo-corp.jp>
2014-04-17 19:30:11 +0900

    Add user microposts

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -29,11 +29,6 @@ describe "Authentication" do
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
       before { sign_in user }
-#      before do
-#        fill_in "Email"    , with: user.email.upcase
-#        fill_in "Password" , with: user.password
-#        click_button "Sign in"
-#      end
 
       it { should have_title(user.name) }
       it { should have_link('Users',    href: users_path) }
@@ -84,6 +79,18 @@ describe "Authentication" do
         describe "visiting the user index" do
           before { visit users_path }
           it { should have_title('Sign in') }
+        end
+      end
+
+      describe "in the Microposts controller" do
+        describe "submitting to the create action" do
+          before { post microposts_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete micropost_path(FactoryGirl.create(:micropost)) }
+          specify { expect(response).to redirect_to(signin_path) }
         end
       end
     end
